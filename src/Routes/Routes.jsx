8e3de67c662acc10e260fce570a96304f29ab719
CoderShawon <shawonb500@gmail.com>
2023-05-18 11:23:12 +0600

    Route setup done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main";
+import Home from "../Pages/Home/Home/Home";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+    //   errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+      ],
+    },
+  ]);
